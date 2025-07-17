4166cf20c1af1d266c5c8a6d9ce76cbeefc3de12
Abner Pereira <abnerluiz.88@gmail.com>
2025-07-17 18:45:59 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/bash
+#!/bin/bash
+# Variáveis
+MDIRSRC="./br"
+MFILEJAR="calculadora.jar"
+# Início
 echo "Log: Iniciando compilação."
 rm -rf ./out
 cd ./src
-MDIRSRC="./br"
-# Opção 1 - Todos os arqquivos dentro da pasta "$MDIRSRC 
+# Opção 1 - Todos os arquivos dentro da pasta "$MDIRSRC 
 javac $MDIRSRC/**/*.java -d ../out --release 21
 # Opção 2 - Todos os arquivos definidos manualmente (pasta-a-pasta)
 #javac $MDIRSRC/calc/*.java -d ../out --release 21
@@ -13,12 +16,12 @@ echo "Log: Compilação concluída."
 echo "Log: Criando JAR."
 rm -rf ./artefact
 cd ./out
-# Opção 1 - Todos os arqquivos dentro da pasta "$MDIRSRC 
-jar -c -e br.main.Main --file ../artefact/calculadora.jar $MDIRSRC/**/*.class 
+# Opção 1 - Todos os arquivos dentro da pasta "$MDIRSRC 
+jar -c -e br.main.Main --file ../artefact/$MFILEJAR $MDIRSRC/**/*.class 
 # Opção 2 - Todos os arquivos definidos manualmente (pasta-a-pasta)
-#jar -c -e br.main.Main --file ../artefact/calculadora.jar $MDIRSRC/calc/*.class $MDIRSRC/main/*.class
+#jar -c -e br.main.Main --file ../artefact/$MFILEJAR $MDIRSRC/calc/*.class $MDIRSRC/main/*.class
 cd ..
 echo "Log: JAR criado."
 echo "Log: Iniciando execução."
-java -jar ./artefact/calculadora.jar
+java -jar ./artefact/$MFILEJAR
 echo "Log: Execução concluída."
